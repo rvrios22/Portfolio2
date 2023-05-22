996e3e9ed76c8cb202d1d3e726e5dcc850d1d166
Rafael Rios <rvrios22@gmail.com>
2023-05-22 15:19:11 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import Landing from './components/landing/landing';
+import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact';
+
+function App() {
+
+  return (
+    <>
+      <Landing />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
+
+export default App
